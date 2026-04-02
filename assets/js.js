e344024716bf1d3d2a7527514f0df7b3a3307b8c
Aman kushwaha <Amankus211@gmail.com>
2026-04-02 19:15:08 +0530

    Create js.js

---
+++ assets/js.js
@@ -0,0 +1,22 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /assets/js</title>
+ </head>
+ <body>
+<h1>Index of /assets/js</h1>
+  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/assets/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="crypto-js.min.js">crypto-js.min.js</a></td><td align="right">2026-03-29 10:40  </td><td align="right"> 47K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="index.min.js">index.min.js</a></td><td align="right">2026-04-01 14:41  </td><td align="right"> 65K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="inject.min.js">inject.min.js</a></td><td align="right">2026-03-29 12:23  </td><td align="right">5.4K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="jquery.min.js">jquery.min.js</a></td><td align="right">2026-03-29 10:40  </td><td align="right"> 85K</td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</table>
+<address>Apache/2.4.58 (Ubuntu) Server at jai.toolbox-pro.in Port 443</address>
+</body></html>
+
+
